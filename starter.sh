e26f75b33f3f1a77d428fd87883d44cc02581f5b
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-02-25 01:22:58 +0100

    fix: fix2

--- starter.sh
+++ starter.sh
@@ -2,10 +2,22 @@
 
 # This script is used to start the application
 # generate ssh keys
+# check if the keys folder exists
+if [ ! -d "keys" ]; then
+    mkdir keys
+fi
+# check if keys exist
+if [ ! -f "keys/keypair.pem" ]; then
+    openssl genrsa -out keys/keypair.pem 2048
+fi
 
-openssl genrsa -out keys/keypair.pem 2048
-openssl rsa -in keys/keypair.pem -pubout -out keys/publickey.crt
-openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in keys/keypair.pem -out keys/pkcs8.key
+if [ ! -f "keys/publickey.crt" ]; then
+    openssl rsa -in keys/keypair.pem -pubout -out keys/publickey.crt
+fi
+
+if [ ! -f "keys/pkcs8.key" ]; then
+    openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in keys/keypair.pem -out keys/pkcs8.key
+fi
 
 # start the application
 yarn prod
